0b10f508aef67a5d0fed8e037e76be8756b9dabe
Sangzh1 <Kangseobon@gmail.com>
2024-01-20 16:14:29 +0500

    file has been actived

---
+++ js/index.js
@@ -0,0 +1,81 @@
+  //no_1
+
+ let son = +prompt("Ixtiyoriy sonini kiriting");
+ let s ;
+ for(let s=1; s<=son; s +=1){
+     console.log(s);
+}
+
+ //no_2
+ let num_1= +prompt("Ixtiyoriy sonni kiritng");
+ let num_2 = 0;
+ let = a;
+ for (a=1; a<=num_1; a+=1){
+   num2+=a;
+ }
+ document.write("Yig'indi: " + num2);
+
+//no_3
+
+ let number= +prompt(" Ixtiyoriy sonni kiritng");
+ let sum$ = 0;
+ let Num;
+ for (Num=1; Num<=number; Num+=1){
+   if (Num% 2 == 1){
+     sum$+=Num;
+   }
+ }
+ console.log("Yig'indi: " + sum$);
+
+ //No_4
+
+ let son_1 = +prompt(`Boshlangich sonni kiriting`);
+ let son_2 = +prompt(`so'nggi sonni kiriting`);
+ let result=1;
+ let son_3;
+ for (son_3 = son; son_3 <= son_2; son_3 += 1) {
+   if (son_3 % 2 == 0) {
+     result *= son_3;
+   }
+ }
+ document.write(`Ko'paytmasi:`+ result)
+
+ //No_5
+
+ let Son_1 = +prompt(`Boshlang'ich sonni kiriting`);
+ let Son_2 = +prompt(`so'nggi sonni kiriting`);
+ let res =0;
+ let a;
+ for (a = Son_1; a <= Son_2; a += 1) {
+   if (a % 3 == 0 && a % 5 == 0) {
+     res += a;
+   }
+ }
+ document.write(`Yig'indi:` + res);
+
+ //no_6
+ let Num$ = +prompt(`Ixtiyoriy sonni kiriting`);
+ let son_;
+ let Res;
+ let res_1=0;
+ for (son_ = 1; son_ <= Num$; son_ += 1) {
+   if (Num$ % son_ == 0) {
+     res_1 += 1;
+     Res = res_1;
+   }
+ }
+ document.write(`Yig'indi:` + Res);
+
+
+ //no_7
+
+let So_n = +prompt(`Ixtiyoriy sonni kiriting`);
+let i;
+let resul_t$;
+for (i = 1; i <= So_n; i += 1) {
+  if (So_n % i == 0) {
+  
+  }
+}
+document.write(`Yig'indi:` + resul_t$);
+
